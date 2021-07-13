8b1a496a8843e0755548f9bb6f1c4977484daf95
Oliver Vazquez <olivervazquez@gmail.com>
2021-07-13 03:03:17 -0400

    Addeed css for privacy/about, updated button

--- client/src/components/sections/Privacy.js
+++ client/src/components/sections/Privacy.js
@@ -6,6 +6,20 @@ const Privacy = () => {
     return (
         <div className="privacy-container">
             <h1 className="privacy-title">Privacy</h1>
+            <div className="privacy-text-container">
+                <p className="privacy-text">
+                    At vero eos et accusamus et iusto odio dignissimos ducimus
+                    qui blanditiis praesentium voluptatum deleniti atque
+                    corrupti quos dolores et quas molestias excepturi sint
+                    occaecati cupiditate non provident, similique sunt in culpa
+                    qui officia deserunt mollitia animi, id est laborum et
+                    dolorum fuga. Et harum quidem rerum facilis est et expedita
+                    distinctio. Nam libero tempore, cum soluta nobis est
+                    eligendi optio cumque nihil impedit quo minus id quod maxime
+                    placeat facere possimus, omnis voluptas assumenda est, omnis
+                    dolor repellendus.
+                </p>
+            </div>
             <Footer first="login" second="about" />
         </div>
     );
